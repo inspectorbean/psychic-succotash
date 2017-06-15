8317142688759d71d6f822758e0936bec854fced
oakea <erkoak@gmail.com>
2017-06-14 20:34:37 -0400

    bug fix

--- bus/models.py
+++ bus/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class File(models.Models):
+class File(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     path = models.TextField()
 
