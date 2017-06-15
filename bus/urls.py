b0cf2badae2aabba3e3ad0fc6225922a3a0a2a40
oakea <erkoak@gmail.com>
2017-06-14 20:52:36 -0400

    bug fix

--- bus/urls.py
+++ bus/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^data/$', views.data, name='data'),
+    #url(r'^data/$', views.data, name='data'),
     url(r'^$', views.bus, name='bus'),
 ]
