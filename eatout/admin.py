685f953ee822b933ea89d5691c8fb0998d600181
inspectorbean <erkoak@gmail.com>
2017-11-05 12:37:19 -0500

    admin

--- eatout/admin.py
+++ eatout/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Dinner, Restaurant, Mood
+
+admin.site.register(Dinner)
+admin.site.register(Restaurant)
+admin.site.register(Mood)
